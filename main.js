ac4ac71312cdcbba54c36098721dfa30bb6086f7
ritikrrm999-debug <ritikrrm999@gmail.com>
2025-09-06 19:58:26 +0530

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+// Firebase initialization using environment variables
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+console.log("Firebase initialized safely!");
+
+// You can add your app logic here
