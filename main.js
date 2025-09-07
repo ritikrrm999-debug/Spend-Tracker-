130f966797486cae0c5862507e30f4e885efadd2
ritikrrm999-debug <ritikrrm999@gmail.com>
2025-09-07 06:28:48 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-// Firebase config using environment variables
+// Firebase config (placeholders will be replaced by GitHub Actions)
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID
+  apiKey: "${FIREBASE_API_KEY}",
+  authDomain: "${FIREBASE_AUTH_DOMAIN}",
+  projectId: "${FIREBASE_PROJECT_ID}",
+  storageBucket: "${FIREBASE_STORAGE_BUCKET}",
+  messagingSenderId: "${FIREBASE_MESSAGING_SENDER_ID}",
+  appId: "${FIREBASE_APP_ID}"
 };
 
 // Initialize Firebase
